704c1a79d3799e20894a4a154213ce143d08f1a4
pooooz <antoivans@gmail.com>
2024-01-04 02:55:35 +0100

    feat: move folders

--- backend/src/constants/envMapping.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS;
-
-export const CONNECTION_STRING = process.env.CONNECTION_STRING;
-export const PORT = process.env.PORT;
-export const JWT_SECRET = process.env.JWT_SECRET;
